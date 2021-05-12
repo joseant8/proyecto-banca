bc8d5520f1a8a96bde2e604bb95cca0b374dfc1d
joseant8 <j2a2mari@gmail.com>
2021-05-12 13:19:53 +0200

    añado plantilla controladores

---
+++ src/main/java/com/ingenia/banca/controller/CuentaController.java
@@ -0,0 +1,9 @@
+package com.ingenia.banca.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/API")
+public class CuentaController {
+}
