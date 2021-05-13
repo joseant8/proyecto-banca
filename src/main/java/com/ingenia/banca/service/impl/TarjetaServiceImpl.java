51713238c8530c07f16c7a37385210fcd4da87d0
jsgonzalez <jsgonzalez@ingenia.es>
2021-05-13 18:14:00 +0200

    Creado Implementacion de endpoints de Tarjeta

--- src/main/java/com/ingenia/banca/service/impl/TarjetaServiceImpl.java
+++ src/main/java/com/ingenia/banca/service/impl/TarjetaServiceImpl.java
@@ -1,8 +1,45 @@
 package com.ingenia.banca.service.impl;
 
+import com.ingenia.banca.model.Cuenta;
+import com.ingenia.banca.model.Tarjeta;
+import com.ingenia.banca.repository.TarjetaRepository;
 import com.ingenia.banca.service.TarjetaService;
+
+import java.util.List;
+import java.util.Optional;
+
+import javax.persistence.EntityNotFoundException;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class TarjetaServiceImpl implements TarjetaService {
+	
+	@Autowired
+	TarjetaRepository tarjetaRepository;
+
+	@Transactional
+    @Override
+	public Tarjeta crearTarjeta(Tarjeta tarjetaNueva) {
+		return tarjetaRepository.save(tarjetaNueva);
+	}
+
+	@Transactional
+	@Override
+	public List<Tarjeta> obtenerTarjetaByCuenta(Cuenta cuenta) {
+		Long id = cuenta.getId();
+		return tarjetaRepository.findByCuentaId(id);
+	}
+
+	@Transactional
+	@Override
+	public Tarjeta obtenerTarjetaByNumeroTarjeta(Long numeroTarjeta) {
+		return tarjetaRepository.obtenerTarjetaByNumeroTarjeta(numeroTarjeta).orElseThrow(()
+                -> new EntityNotFoundException("No se ha encontrado tarjeta con numero: "+numeroTarjeta));
+	}
+	
+	
+	
 }
