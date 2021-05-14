07a3d78c5261371dbff6ef7a1754502db65bd6fb
joseant8 <j2a2mari@gmail.com>
2021-05-14 22:32:33 +0200

    seguridad

---
+++ src/main/java/com/ingenia/banca/security/service/UserDetailsServiceImpl.java
@@ -0,0 +1,30 @@
+package com.ingenia.banca.security.service;
+
+import com.ingenia.banca.model.Usuario;
+import com.ingenia.banca.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+/**
+ * Autentica un usuario de la base de datos
+ */
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Usuario usuario = usuarioRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuario no encontrado: " + username));
+
+        return new org.springframework.security.core.userdetails.User(
+                usuario.getUsername(),usuario.getPassword(),new ArrayList<>());
+    }
+}
