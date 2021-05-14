2d6d16ed19188bfe5559f7d04366d5b12eab3455
jsgonzalez <jsgonzalez@ingenia.es>
2021-05-14 11:08:49 +0200

    Borrado servicios no funcionales

--- src/main/java/com/ingenia/banca/service/TarjetaService.java
+++ src/main/java/com/ingenia/banca/service/TarjetaService.java
@@ -1,22 +1,22 @@
-package com.ingenia.banca.service;
-
-import java.util.List;
-import java.util.Optional;
-
-import org.springframework.stereotype.Service;
-
-import com.ingenia.banca.model.Cuenta;
-import com.ingenia.banca.model.Tarjeta;
-
-@Service
-public interface TarjetaService {
-	
-	public Tarjeta crearTarjeta(Tarjeta tarjetaNueva);
-	
-	public List<Tarjeta> obtenerTarjetaByCuenta(Cuenta cuenta);
-
-	public Tarjeta obtenerTarjetaByNumeroTarjeta(Long numeroTarjeta);
-	
-	public Tarjeta obtenerTarjetaById(Long idTarjeta);
-	
-}
+//package com.ingenia.banca.service;
+//
+//import java.util.List;
+//import java.util.Optional;
+//
+//import org.springframework.stereotype.Service;
+//
+//import com.ingenia.banca.model.Cuenta;
+//import com.ingenia.banca.model.Tarjeta;
+//
+//@Service
+//public interface TarjetaService {
+//	
+//	public Tarjeta crearTarjeta(Tarjeta tarjetaNueva);
+//	
+//	public List<Tarjeta> obtenerTarjetaByCuenta(Cuenta cuenta);
+//
+//	public Tarjeta obtenerTarjetaByNumeroTarjeta(Long numeroTarjeta);
+//	
+//	public Tarjeta obtenerTarjetaById(Long idTarjeta);
+//	
+//}
