21d7e609b61c8402e34fd9fb6e2c5fe7656e4368
joseant8 <j2a2mari@gmail.com>
2021-05-14 22:31:54 +0200

    peticiones autenticación

---
+++ src/main/java/com/ingenia/banca/payload/request/SignupRequest.java
@@ -0,0 +1,32 @@
+package com.ingenia.banca.payload.request;
+
+public class SignupRequest {
+
+    private String nombreCompleto;
+    private String username;
+    private String password;
+
+    public String getNombreCompleto() {
+        return nombreCompleto;
+    }
+
+    public void setNombreCompleto(String nombreCompleto) {
+        this.nombreCompleto = nombreCompleto;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
