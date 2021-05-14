c548989b64e92abd3999aa773d52e06290953c64
joseant8 <j2a2mari@gmail.com>
2021-05-14 10:40:47 +0200

    nuevos métodos en repositorio usuario

--- src/main/java/com/ingenia/banca/repository/UsuarioRepository.java
+++ src/main/java/com/ingenia/banca/repository/UsuarioRepository.java
@@ -2,6 +2,10 @@ package com.ingenia.banca.repository;
 
 import com.ingenia.banca.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+    Optional<Usuario> findByUsername(String username);
+
 }
